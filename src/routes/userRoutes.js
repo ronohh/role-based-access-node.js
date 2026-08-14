b924053993978f5d619f148497deb0e9372e41da
Ronohh <cimanu88@gmail.com>
2026-08-14 13:34:35 +0300

    access based on user role control api

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,21 +1,22 @@
 import express from 'express';
 import authMiddleware from '../middlewares/authMiddleware.js';
+import authorizeRoles from '../middlewares/roleMiddleware.js';
 
 const router = express.Router();
 
 // only admin can access this route
 
-router.get('/admin', authMiddleware, (req, res) => {
+router.get('/admin', authMiddleware,authorizeRoles('admin'), (req, res) => {
   res.json({ message: 'Admin route' });
 });
 
 // only manager can accesss this route
-router.get('/manager', authMiddleware, (req, res) => {
+router.get('/manager', authMiddleware,authorizeRoles('admin','manager'), (req, res) => {
   res.json({ message: 'Manager route' });
 });
 
 // only user can access this route
-router.get('/user', authMiddleware, (req, res) => {
+router.get('/user', authMiddleware,authorizeRoles('admin','manager','user'), (req, res) => {
   res.json({ message: 'User route' });
 });
 
